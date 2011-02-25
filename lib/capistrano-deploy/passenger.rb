991cc96d6ef8b3b121f4734535e91c28c5a55399
lest <just.lest@gmail.com>
2011-02-25 16:18:51 +0200

    simply touch file on passenger restart

--- lib/capistrano-deploy/passenger.rb
+++ lib/capistrano-deploy/passenger.rb
@@ -2,7 +2,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :deploy do
     desc 'Restart passenger'
     task :restart, :roles => :app, :except => {:no_release => true} do
-      run "mkdir -p #{deploy_to}/tmp && touch #{deploy_to}/tmp/restart.txt"
+      run "touch #{deploy_to}/tmp/restart.txt"
     end
   end
 end
