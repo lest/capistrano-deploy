0a4248002c40f041d8b4f50b9da0561ab4f0ad55
lest <just.lest@gmail.com>
2011-11-13 22:16:39 +0200

    git specs, deploy task spec

--- spec/deploy_spec.rb
+++ spec/deploy_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'core' do
+describe 'deploy' do
   before do
     mock_config { use_recipes :git, :rails }
   end
@@ -12,5 +12,13 @@ describe 'core' do
   it 'checks if recipe is used' do
     config.should be_using_recipe(:git)
     config.should_not be_using_recipe(:bundle)
+  end
+
+  describe 'deploy' do
+    it 'runs update and restart' do
+      config.namespaces[:deploy].should_receive(:update)
+      config.namespaces[:deploy].should_receive(:restart)
+      cli_execute 'deploy'
+    end
   end
 end
--- spec/git_spec.rb
+++ spec/git_spec.rb
@@ -5,8 +5,57 @@ describe 'git' do
     mock_config { use_recipe :git; set :deploy_to, '/foo/bar' }
   end
 
-  it 'sets application from repo' do
-    config.set :repository, 'git@example.com/test-app.git'
-    config.application.should == 'test-app'
+  it 'has branch' do
+    config.branch.should == 'master'
+  end
+
+  context 'with repository' do
+    before do
+      mock_config { set :repository, 'git@example.com/test-app.git' }
+    end
+
+    it 'sets application from repo' do
+      config.application.should == 'test-app'
+    end
+
+    describe 'deploy:setup' do
+      it 'clones repository' do
+        cli_execute 'deploy:setup'
+        config.should have_run('mkdir -p `dirname /foo/bar` && git clone --no-checkout git@example.com/test-app.git /foo/bar')
+      end
+
+      it 'invokes update during setup' do
+        config.namespaces[:deploy].should_receive(:update)
+        cli_execute 'deploy:setup'
+      end
+    end
+
+    describe 'deploy:update' do
+      it 'updates' do
+        cli_execute 'deploy:update'
+        config.should have_run('cd /foo/bar && git fetch origin && git reset --hard origin/master')
+      end
+
+      it 'updates submodules' do
+        mock_config { set :enable_submodules, true }
+        cli_execute 'deploy:update'
+        config.should have_run('cd /foo/bar && git fetch origin && git reset --hard origin/master && git submodule init && git submodule -q sync && git submodule -q update')
+      end
+    end
+  end
+
+  it 'has current revision' do
+    config.should_receive(:capture).with('cd /foo/bar && git rev-parse HEAD') { "baz\n" }
+    config.current_revision.should == 'baz'
+  end
+
+  it 'shows pending' do
+    config.should_receive(:current_revision) { 'baz' }
+    config.namespaces[:deploy].should_receive(:system).with('git log --pretty=medium --stat baz..origin/master')
+    cli_execute 'deploy:pending'
+  end
+
+  it 'sets forward agent' do
+    config.ssh_options[:forward_agent].should == true
   end
 end
