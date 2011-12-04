2f7c8004540dfd4a15f0390b6248ff8a29c6114c
Sergey Nartimov <just.lest@gmail.com>
2011-12-04 17:43:30 +0300

    whenever recipe spec

---
+++ spec/whenever_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe 'whenever' do
+  before do
+    mock_config { use_recipe :whenever}
+  end
+
+  describe 'whenever_identifier' do
+    before do
+      mock_config { set :application, 'foo' }
+    end
+
+    it 'defaults to application' do
+      config.whenever_identifier.should == 'foo'
+    end
+
+    it 'default to application with stage when using multistage' do
+      mock_config do
+        use_recipe :multistage
+        set :stage, 'bar'
+      end
+      config.whenever_identifier.should == 'foo_bar'
+    end
+  end
+end
