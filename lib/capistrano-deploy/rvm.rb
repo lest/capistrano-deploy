de38e6afd7f441f74a0f845962999ad8c09a8fd2
Sergey Nartimov <just.lest@gmail.com>
2012-01-05 21:06:53 +0300

    don't depend on rvm installed on local machine

--- lib/capistrano-deploy/rvm.rb
+++ lib/capistrano-deploy/rvm.rb
@@ -2,8 +2,18 @@ module CapistranoDeploy
   module Rvm
     def self.load_into(configuration)
       configuration.load do
-        $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-        require 'rvm/capistrano'
+        set :rvm_ruby_string, 'default'
+        set :rvm_path, '/usr/local/rvm'
+
+        set(:rvm_shell_path) { "#{rvm_path}/bin/rvm-shell" }
+
+        set :default_shell do
+          shell = rvm_shell_path
+          ruby  = rvm_ruby_string.to_s.strip
+          shell = "rvm_path=#{rvm_path} #{shell} '#{ruby}'" unless ruby.empty?
+
+          shell
+        end
 
         if File.exists?('.rvmrc')
           matches = File.read('.rvmrc').scan(/^rvm\s+use\s+.*?([\w\-\.]+@[\w\-]+).*$/)
