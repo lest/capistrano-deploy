b123db91da81855b89f49156e7e6eae487d6d001
Sergey Nartimov <just.lest@gmail.com>
2012-01-08 15:48:16 +0300

    add descriptions to assets tasks

--- lib/capistrano-deploy/rails_assets.rb
+++ lib/capistrano-deploy/rails_assets.rb
@@ -6,10 +6,12 @@ module CapistranoDeploy
 
         namespace :deploy do
           namespace :assets do
+            desc 'Precompile assets'
             task :precompile do
               run "cd #{deploy_to} && RAILS_ENV=#{rails_env} RAILS_GROUPS=assets #{rake} assets:precompile"
             end
 
+            desc 'Clean assets'
             task :clean do
               run "cd #{deploy_to} && RAILS_ENV=#{rails_env} RAILS_GROUPS=assets #{rake} assets:clean"
             end
