365e4a99d223aa89690db5d121263e2d8236e495
Sergey Nartimov <just.lest@gmail.com>
2012-01-14 02:37:06 +0300

    no need in class_eval on singleton class

--- lib/capistrano-deploy.rb
+++ lib/capistrano-deploy.rb
@@ -5,17 +5,17 @@ module CapistranoDeploy
     configuration.load do
       @used_recipes = []
 
-      (class << self; self; end).class_eval do
+      class << self
         attr_reader :used_recipes
+      end
 
-        define_method :use_recipe do |recipe_name|
-          return if @used_recipes.include?(recipe_name.to_sym)
-          @used_recipes << recipe_name.to_sym
+      def use_recipe(recipe_name)
+        return if @used_recipes.include?(recipe_name.to_sym)
+        @used_recipes << recipe_name.to_sym
 
-          require "capistrano-deploy/#{recipe_name}"
-          recipe = CapistranoDeploy.const_get(recipe_name.to_s.capitalize.gsub(/_(\w)/) { $1.upcase })
-          recipe.load_into(configuration)
-        end
+        require "capistrano-deploy/#{recipe_name}"
+        recipe = CapistranoDeploy.const_get(recipe_name.to_s.capitalize.gsub(/_(\w)/) { $1.upcase })
+        recipe.load_into(self)
       end
 
       def use_recipes(*recipes)
