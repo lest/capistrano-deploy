c1259630949883a2b2eee6ab0f6b09aae2908623
Michael McClenaghan <mike@sideline.ca>
2012-03-31 17:39:47 -0600

    Fixed a bug

--- lib/capistrano-deploy/foreman.rb
+++ lib/capistrano-deploy/foreman.rb
@@ -13,22 +13,22 @@ module CapistranoDeploy
         set(:rake) { "#{foreman_cmd} run rake" }
         set(:foreman_format) { 'upstart' }
         foreman_env_files = fetch(:foreman_env_files, '.env')        
-      end
 
-      namespace :foreman do
-        desc 'Start foreman'
-        task :start, :roles => :app, :except => {:no_release => true} do
-          run "cd #{deploy_to}; #{foreman_cmd} start"
-        end
+        namespace :foreman do
+          desc 'Start foreman'
+          task :start, :roles => :app, :except => {:no_release => true} do
+            run "cd #{deploy_to}; #{foreman_cmd} start"
+          end
 
-        desc 'Export foreman'
-        task :export, :roles => :app, :except => {:no_release => true} do
-          run "cd #{deploy_to}; #{foreman_cmd} export #{foreman_format} /home/#{user}/service --app=#{app_name} --user=#{user} --log=#{deploy_to}/log --env=#{foreman_env_files}"
-        end
+          desc 'Export foreman'
+          task :export, :roles => :app, :except => {:no_release => true} do
+            run "cd #{deploy_to}; #{foreman_cmd} export #{foreman_format} /home/#{user}/service --app=#{app_name} --user=#{user} --log=#{deploy_to}/log --env=#{foreman_env_files}"
+          end
 
-        desc 'Upload .env file'
-        task :upload_env, :roles => :app, :except => {:no_release => true} do
-          upload foreman_env_files, "#{deploy_to}/"
+          desc 'Upload .env file'
+          task :upload_env, :roles => :app, :except => {:no_release => true} do
+            upload foreman_env_files, "#{deploy_to}/"
+          end
         end
       end
     end
