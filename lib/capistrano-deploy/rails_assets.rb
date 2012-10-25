cec62fb1d7521a635f629ae4f58e854e50aa825a
Mike McClenaghan <mike@sideline.ca>
2012-10-25 15:22:08 -0600

    Added some dirty_asset checking so that assets only get precompiled if they've actually changed

--- lib/capistrano-deploy/rails_assets.rb
+++ lib/capistrano-deploy/rails_assets.rb
@@ -3,18 +3,33 @@ module CapistranoDeploy
     def self.load_into(configuration)
       configuration.load do
         use_recipe :rails
+        use_recipe :git
+
+        set(:asset_paths) { ["vendor/assets/", "app/assets/", "lib/assets", "Gemfile", "Gemfile.lock"].join(" ")}
 
         namespace :deploy do
           namespace :assets do
             desc 'Precompile assets'
             task :precompile do
-              run "cd #{deploy_to} && RAILS_ENV=#{rails_env} RAILS_GROUPS=assets #{rake} assets:precompile"
+              if force_asset_compilation? || assets_dirty?
+                run "cd #{deploy_to} && RAILS_ENV=#{rails_env} RAILS_GROUPS=assets #{rake} assets:precompile"
+              else
+                logger.info "Skipping asset pre-compilation because there were no asset changes"
+              end                
             end
 
             desc 'Clean assets'
             task :clean do
               run "cd #{deploy_to} && RAILS_ENV=#{rails_env} RAILS_GROUPS=assets #{rake} assets:clean"
             end
+                        
+            def force_asset_compilation?
+              fetch(:always_compile_assets, false) || ENV['COMPILE_ASSETS'] == 'true'
+            end
+
+            def assets_dirty?
+              capture("cd #{deploy_to} && git log #{current_revision}..origin/#{branch} #{asset_paths} | wc -l").to_i > 0
+            end            
           end
         end
       end
